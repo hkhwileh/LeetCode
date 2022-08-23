e1bedbdcc7ee335b3b8f7123a3c003b60cf67be0
hkhwileh <hassan.khwileh@gmail.com>
2022-08-23 23:14:20 +0400

    Time: 190 ms (40.15%), Space: 14.2 MB (89.46%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,20 +1,8 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         #To understand the approach better let us use the same example for better understanding. Count the number of ones in each row. 
-        ones = []
-        indexs = []
-        
-        for index , val in enumerate(mat):
-            cnt = 0
-            for v in val:
-                if v == 1:
-                    cnt +=1
-            
-            ones.append((cnt,index))
-        print(ones)
-        ones = sorted(ones)
-        
-        for i in range(k):
-            indexs.append(ones[i][1])
-        
-        return indexs
+        ans={}
+        for i in range(len(mat)):
+            ans[i]=sum(mat[i])
+        ans=sorted(ans, key=ans.get)
+        return ans[:k]
